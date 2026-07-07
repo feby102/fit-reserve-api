7f5e8b21df9d0a3aa3965a78a4c4010606cff843
Feby Emad <febyemad102@gmail.com>
2026-07-07 13:26:50 +0300

    Update project with new changes

--- app/Models/LedgerEntry.php
+++ app/Models/LedgerEntry.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class LedgerEntry extends Model
 {
+    protected $table = 'LedgerEntry'; 
     protected $fillable = [
         'account_type',
         'account_id',
